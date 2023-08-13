c9496d626c16f24ea13737d2c0f24f0f74c6ae9e
ania.liulka <anialiulka@gmail.com>
2023-08-13 11:02:56 -0400

    refactored React components to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Input } from './components/Input';
 import { Title } from './Title';
@@ -8,32 +8,18 @@ import { ContactElement } from 'components/ContactElement/ContactElement';
 
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter] = useState('');
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      try {
-        const parsedContacts = JSON.parse(savedContacts);
-        this.setState({ contacts: parsedContacts });
-      } catch (error) {
-        console.log(error.name, error.message);
-      }
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  });
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = data => {
-    const nameRepeated = this.state.contacts.find(
+  const addContact = data => {
+    const nameRepeated = contacts.find(
       contact => contact.name.toLowerCase() === data.name.toLowerCase()
     );
     if (nameRepeated) {
@@ -43,45 +29,38 @@ export class App extends Component {
       ...data,
       id: nanoid(),
     };
-    this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts],
-    }));
+    setContacts(prevState => [newContact, ...prevState]);
   };
 
-  handleChange = ({ target }) => {
-    this.setState({
+  const handleChange = ({ target }) => {
+    setContacts({
       [target.name]: target.value,
     });
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  deleteContact = id => {
-    const updatedContacts = this.state.contacts.filter(
-      contact => contact.id !== id
-    );
-    return this.setState({ contacts: updatedContacts });
+  const deleteContact = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
   };
 
-  render() {
-    return (
-      <div className={css.container}>
-        <Title>Phonebook</Title>
-        <Input addContact={this.addContact} />
-        <Title>Contacts</Title>
-        <Filter handleChange={this.handleChange} filter={this.state.filter} />
-        <Contacts>
-          <ContactElement
-            contacts={this.getFilteredContacts()}
-            deleteContact={this.deleteContact}
-          />
-        </Contacts>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.container}>
+      <Title>Phonebook</Title>
+      <Input addContact={addContact} />
+      <Title>Contacts</Title>
+      <Filter handleChange={handleChange} filter={filter} />
+      <Contacts>
+        <ContactElement
+          contacts={getFilteredContacts()}
+          deleteContact={deleteContact}
+        />
+      </Contacts>
+    </div>
+  );
+};
--- src/components/FIlter/index.jsx
+++ src/components/FIlter/index.jsx
@@ -1,25 +1,21 @@
-import { Component } from 'react';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-export class Filter extends Component {
-  render() {
-    const { handleChange, filter } = this.props;
-    return (
-      <label htmlFor="filter" className={css.label}>
-        Find contacts by name
-        <input
-          className={css.input}
-          type="text"
-          name="filter"
-          required
-          onChange={handleChange}
-          value={filter}
-        />
-      </label>
-    );
-  }
-}
+export const Filter = ({ handleChange, filter }) => {
+  return (
+    <label htmlFor="filter" className={css.label}>
+      Find contacts by name
+      <input
+        className={css.input}
+        type="text"
+        name="filter"
+        required
+        onChange={handleChange}
+        value={filter}
+      />
+    </label>
+  );
+};
 
 Filter.propTypes = {
   handleChange: PropTypes.func.isRequired,
